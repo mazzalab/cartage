b73acd31a921e818adf939a3e44f0df760bf8a12
t.mazza <t.mazza@css-mendel.it>
2019-12-14 10:12:18 +0100

    Setup the blank project and configuration files

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HWP = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    // entry: path.join(__dirname, '/src/index.js'),
+    entry: './src/index.js',
+    output: {
+        filename: 'build.js',
+        //path: path.join(__dirname, '/dist')
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/'
+    },
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env', "@babel/preset-react"]
+                  }
+            }
+        }]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+      },
+    plugins: [
+        new HWP(
+            { template: path.resolve(__dirname, './dist/index.html') }
+        ),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    devServer: {
+        contentBase: './dist',
+        hot: true
+      }
+}
